f90e26e2bd906be29d62fdcbeba9e8a13354ae88
salmaii <bile.slamai@gmail.com>
2020-02-13 12:07:57 -0300

    help commit

--- myrepofolder/a.c
+++ myrepofolder/a.c
@@ -1,7 +1,37 @@
-#include <stdio.h>
+/*
+Para fazer novos commits:
 
-int main(int argc, char const *argv[])
-{
-    printf("aaaa");
-    return 0;
-}
+->terminar o trabalho 
+->ir no source control 
+->adicionar as novas versões
+->adicionar o comentário 
+->clicar command+enter
+->fazer o push request
+*/
+
+//-----------------------------
+
+/*
+Para receber mudanças em outro local:
+
+->ir no source control
+->fazer o pull request
+*/
+
+//-----------------------------
+
+/*
+
+Para fazer o primeiro commit
+
+->criar repositórios etc...
+->adicionar ele a uma pasta do computador local
+->vincular ela com command+shift+p 
+->git clone 
+->colar a url 
+
+
+
+->
+
+*/
--- myrepofolder/b.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("aaaa");
-    return 0;
-    /*aaaaa*/
-}
