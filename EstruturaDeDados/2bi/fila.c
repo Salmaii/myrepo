974075620021d2a976310573eca72dda6e77db88
salmaii <bile.slamai@gmail.com>
2020-10-25 15:24:10 -0300

    a

---
+++ EstruturaDeDados/2bi/fila.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+
+   int tam=3;
+   int *vet;
+   vet=(int*)malloc(sizeof(vet));
+   
+
+   
+
+   return 0;
+}
